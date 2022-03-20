b853b44da89afa9ad2ebac439474b108e4f78017
VasiliCheb <skimanvasia@gmail.com>
2022-03-20 17:11:23 +0300

    Правда или действие (часть 1) исправления

--- js/form.js
+++ js/form.js
@@ -3,18 +3,18 @@ import {isEscapeKey} from './util.js';
 
 const form = document.querySelector('#upload-select-image');
 
-new Pristine(form);
+const pristine = new Pristine(form);
 
 const imgFile = form.querySelector('#upload-file');
 const imgEditor = form.querySelector('.img-upload__overlay');
 const closeButton = imgEditor.querySelector('#upload-cancel');
-const textAreaComment = imgEditor.querySelector('.text__description');
+//const textAreaComment = imgEditor.querySelector('.text__description');
 
-const textAreaCommentEsc = (evt) => {
-  if (textAreaComment.checked ) {
+/*const textAreaCommentEsc = (evt) => {
+  if (textAreaComment.onfocus) {
     evt.stopPropagation();
   }
-};
+};*/
 
 const closeEditorForm = () => {
   imgEditor.classList.add('hidden');
@@ -30,7 +30,6 @@ const onEditorFormEscKeydown = (evt) => {
   if (isEscapeKey(evt)) {
     closeEditorForm();
   }
-  textAreaCommentEsc();
 };
 
 imgFile.addEventListener('change', () => {
@@ -39,3 +38,34 @@ imgFile.addEventListener('change', () => {
   closeButton.addEventListener('click', onCloseEditorForm);
   document.addEventListener('keydown', onEditorFormEscKeydown);
 });
+
+const re = /^#[A-Za-zА-Яа-яЁё0-9]{1,19}$/;
+
+/*function validateNickname (value) {
+  return value.length >= 2 && value.length <= 50;
+}*/
+const hashtagsText = form.querySelector('.text__hashtags').value;
+
+const hashtagsGroup = hashtagsText.split(',', 5);
+
+//хэш-теги нечувствительны к регистру: #ХэшТег и #хэштег считаются одним и тем же тегом;
+//хэш-теги разделяются пробелами;
+//один и тот же хэш-тег не может быть использован дважды;
+
+const validateHashtags =() => hashtagsGroup.forEach((hashtag) => hashtag === re.test('JavaScript') && hashtagsGroup.length <= 5);
+
+//const hashtagsGroup = () => re.test('JavaScript') && hashtagsGroup.length <= 5;
+
+pristine.addValidator(hashtagsText, validateHashtags, 'Хэш-тег не соответствует правилам.');
+
+form.addEventListener('submit', (evt) => {
+  evt.preventDefault();
+  pristine.validate();
+});
+
+/*const isValid = pristine.validate();
+if (isValid) {
+  console.log('Можно отправлять');
+} else {
+  console.log('Форма невалидна');
+}*/
